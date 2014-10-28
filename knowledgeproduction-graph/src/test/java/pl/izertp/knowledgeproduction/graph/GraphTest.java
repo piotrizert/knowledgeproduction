e9ce159d0b3a365e2940a0abf82238eedfea5ee7
Piotr Izert <piotr.izert@icompass.pl>
2014-10-28 16:34:06 +0100

    Added new test to the Graph

--- knowledgeproduction-graph/src/test/java/pl/izertp/knowledgeproduction/graph/GraphTest.java
+++ knowledgeproduction-graph/src/test/java/pl/izertp/knowledgeproduction/graph/GraphTest.java
@@ -1,5 +1,6 @@
 package pl.izertp.knowledgeproduction.graph;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -12,6 +13,8 @@ public class GraphTest {
     private static final int FROM1 = 0;
 
     private static final int TO1 = 1;
+
+    private static final int TO2 = 2;
 
     Graph graph;
 
@@ -36,5 +39,13 @@ public class GraphTest {
         graph.addEdge(FROM1, TO1);
         assertTrue(graph.getEdge(FROM1, TO1));
         assertTrue(graph.getEdge(TO1, FROM1));
+    }
+
+    @Test
+    public void testNeighbors() {
+        graph.addEdge(FROM1, TO1);
+        graph.addEdge(FROM1, TO2);
+        assertEquals(2, graph.getNeighbors(FROM1).size());
+        assertEquals(1, graph.getNeighbors(TO1).size());
     }
 }
